da3fd9b012b451a363d298b16595ffe81255b5c8
rjerome <rjerome@architech.ca>
2018-05-02 16:09:34 -0400

    Campaign version

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const ip = require("ip");
 
 http.createServer(function (req, res) {
-  res.write(`********** Latest version of Application Running - Hosted by ${ip.address()} **********\n`);
+  res.write(`********** Campaign version of Application Running - Hosted by ${ip.address()} **********\n`);
   res.end();
 }).listen(8080);
